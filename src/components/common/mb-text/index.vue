c22495c7fdca03b62c2e17ed485c86e2d5793144
nanfs <1272487263@qq.com>
2025-04-11 15:15:14 +0800

    fix(component): 修改 mb-text 组件

--- src/components/common/mb-text/index.vue
+++ src/components/common/mb-text/index.vue
@@ -1,17 +1,36 @@
 <script lang="ts" setup>
 interface Props {
-  text: string | number | null;
-  prefix?: string | number | null;
-  noText?: string;
+  text: string | number | null
+  prefix?: string | null
+  noText?: string
 }
-defineProps<Props>();
+const props = defineProps<Props>()
+
+const hasText = computed(() => {
+  if (props.text !== null && props.text !== undefined && props.text !== '') {
+    return true
+  } else {
+    return false
+  }
+})
+
+const getText = computed(() => {
+  if (!hasText.value) {
+    return ''
+  } else {
+    return props.text
+  }
+})
+
+console.log('hasText', hasText.value)
+console.log('getText', getText.value)
 </script>
 
 <template>
-  <wd-text v-if="text" :="$attrs" :text="text" />
+  <wd-text v-if="hasText" :text="getText" :prefix="prefix" :="$attrs" />
   <view v-else class="flex items-center text-color-4">
     <view>{{ prefix }}</view>
-    <view>{{ noText || "暂无" }}</view>
+    <view>{{ noText ?? '暂无' }}</view>
   </view>
 </template>
 
